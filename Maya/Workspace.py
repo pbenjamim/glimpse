b545820e82cf337c73f087a0347d76539cc0327f
rathyon <sapphiron7@gmail.com>
2019-05-02 22:36:30 +0100

    Add Maya(2019) FileManager

---
+++ Maya/Workspace.py
@@ -0,0 +1,75 @@
+
+def getWorkspaceFileAsString():
+	result = """//Maya 2019 Project Definition
+
+workspace -fr "fluidCache" "maya/fluidCache";
+workspace -fr "JT_ATF" "maya/JT_ATF";
+workspace -fr "images" "../50_renders";
+workspace -fr "offlineEdit" "maya/offlineEdit";
+workspace -fr "STEP_ATF Export" "maya/STEP_ATF Export";
+workspace -fr "furShadowMap" "maya/furShadowMap";
+workspace -fr "INVENTOR_ATF Export" "maya/INVENTOR_ATF Export";
+workspace -fr "SVG" "maya/SVG";
+workspace -fr "scripts" "maya/scripts";
+workspace -fr "STL_ATF" "maya/STL_ATF";
+workspace -fr "DAE_FBX" "../45_caches/fbx";
+workspace -fr "shaders" "maya/shaders";
+workspace -fr "NX_ATF" "maya/NX_ATF";
+workspace -fr "furFiles" "maya/furFiles";
+workspace -fr "CATIAV5_ATF Export" "maya/CATIAV5_ATF Export";
+workspace -fr "OBJ" "../45_caches/obj";
+workspace -fr "alembicCache" "../45_caches/abc";
+workspace -fr "PARASOLID_ATF Export" "maya/PARASOLID_ATF Export";
+workspace -fr "furEqualMap" "maya/furEqualMap";
+workspace -fr "BIF" "maya/BIF";
+workspace -fr "DAE_FBX export" "../45_caches/fbx";
+workspace -fr "CATIAV5_ATF" "maya/CATIAV5_ATF";
+workspace -fr "SAT_ATF Export" "maya/SAT_ATF Export";
+workspace -fr "movie" "../15_misc/flip";
+workspace -fr "move" "maya/move";
+workspace -fr "mayaAscii" "../30_scenes";
+workspace -fr "autoSave" "maya/autoSave";
+workspace -fr "NX_ATF Export" "maya/NX_ATF Export";
+workspace -fr "sound" "../15_misc/audio";
+workspace -fr "mayaBinary" "../30_scenes";
+workspace -fr "timeEditor" "maya/timeEditor";
+workspace -fr "DWG_ATF" "maya/DWG_ATF";
+workspace -fr "JT_ATF Export" "maya/JT_ATF Export";
+workspace -fr "iprImages" "maya/iprImages";
+workspace -fr "renderData" "maya/renderData";
+workspace -fr "CATIAV4_ATF" "maya/CATIAV4_ATF";
+workspace -fr "fileCache" "maya/fileCache";
+workspace -fr "eps" "maya/eps";
+workspace -fr "Fbx" "../45_caches/fbx";
+workspace -fr "3dPaintTextures" "../25_tex";
+workspace -fr "STL_ATF Export" "maya/STL_ATF Export";
+workspace -fr "mel" "maya/mel";
+workspace -fr "translatorData" "maya/translatorData";
+workspace -fr "particles" "maya/particles";
+workspace -fr "scene" "../30_scenes";
+workspace -fr "SAT_ATF" "maya/SAT_ATF";
+workspace -fr "PROE_ATF" "maya/PROE_ATF";
+workspace -fr "WIRE_ATF Export" "maya/WIRE_ATF Export";
+workspace -fr "sourceImages" "../25_tex";
+workspace -fr "clips" "maya/clips";
+workspace -fr "furImages" "maya/furImages";
+workspace -fr "INVENTOR_ATF" "maya/INVENTOR_ATF";
+workspace -fr "STEP_ATF" "maya/STEP_ATF";
+workspace -fr "DWG_ATF Export" "maya/DWG_ATF Export";
+workspace -fr "depth" "maya/depth";
+workspace -fr "sceneAssembly" "maya/sceneAssembly";
+workspace -fr "IGES_ATF Export" "maya/IGES_ATF Export";
+workspace -fr "PARASOLID_ATF" "maya/PARASOLID_ATF";
+workspace -fr "IGES_ATF" "maya/IGES_ATF";
+workspace -fr "teClipExports" "maya/teClipExports";
+workspace -fr "audio" "../15_misc/audio";
+workspace -fr "bifrostCache" "maya/bifrostCache";
+workspace -fr "Alembic" "../45_caches/abc";
+workspace -fr "illustrator" "maya/illustrator";
+workspace -fr "diskCache" "maya/diskCache";
+workspace -fr "WIRE_ATF" "maya/WIRE_ATF";
+workspace -fr "templates" "maya/templates";
+workspace -fr "OBJexport" "../45_caches/obj";
+workspace -fr "furAttrMap" "maya/furAttrMap";"""
+
+	return result
